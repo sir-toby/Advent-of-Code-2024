0fa975504084a23f7dba8b6916f5670e7d9d3ee7
Tobias Bosch <bosch.tobias@outlook.com>
2024-12-16 15:45:30 +0100

    fix pt 1 and solve pt 2

--- day07/day07.py
+++ day07/day07.py
@@ -1,21 +1,23 @@
-import random
+from copy import deepcopy
 def main():
     equations = serializeInput('day07/input.txt')
-    part1(equations)
-    part2()
+    part1(deepcopy(equations))
+    part2(deepcopy(equations))
 
 def part1(equations):
     sumOfCorrectEquations = 0
-    count = 0
     for equation in equations: 
         if evaluate(equation['targetValue'], equation['values'], ['+', '*']) == True: 
             sumOfCorrectEquations += equation['targetValue']
-            count += 1
-    print(sumOfCorrectEquations, count)
+    print(sumOfCorrectEquations)
 
 
-def part2():
-    pass
+def part2(equations):
+    sumOfCorrectEquations = 0
+    for equation in equations: 
+        if evaluate(equation['targetValue'], equation['values'], ['+', '*', '||']) == True: 
+            sumOfCorrectEquations += equation['targetValue']
+    print(sumOfCorrectEquations)
         
 def evaluate(target, numbers, operators):
     openList = [{'value': numbers.pop(0), 'operators': ''}]
@@ -24,12 +26,12 @@ def evaluate(target, numbers, operators):
         openList = addStep(openList, number, operators)
         #print(target, openList)
         for entry in openList: 
-            if entry['value'] == target: 
-                print(entry)
-                return True
-            elif entry['value'] > target: 
+            if entry['value'] > target: 
                 entry['value'] = False
-        
+    for entry in openList:
+        if entry['value'] == target: 
+            print(entry)
+            return True
     return False
 
 def addStep(openList, number, operators): 
@@ -47,6 +49,9 @@ def addStep(openList, number, operators):
                     newPath = path+operator
                 case '*': 
                     newValue = entry['value'] * number
+                    newPath = path+operator
+                case '||':
+                    newValue = int(str(entry['value'])+str(number))
                     newPath = path+operator
             newOpenList.append({'value': newValue, 'operators': newPath})
         
